f6146ff7f3f7590224b8334cf7805b8f6ea8a456
sugvn <suganraj.1211@gmail.com>
2025-08-09 12:32:51 +0530

    change alias nv

--- fish/.config/fish/functions/nv.fish
+++ fish/.config/fish/functions/nv.fish
@@ -1,4 +1,4 @@
-function nv --wraps='NVIM_APPNAME=own_nvim nvim' --description 'alias nv=NVIM_APPNAME=own_nvim nvim'
-  NVIM_APPNAME=own_nvim nvim $argv
+function nv --wraps='NVIM_APPNAME=own_nvim nvim' --wraps='NVIM_APPNAME=old-nvim nvim' --description 'alias nv=NVIM_APPNAME=old-nvim nvim'
+  NVIM_APPNAME=old-nvim nvim $argv
         
 end
