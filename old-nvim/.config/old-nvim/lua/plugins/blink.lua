4339ecad7ec9b5544e9e34cbc70a39c9c859b468
sugvn <suganraj.1211@gmail.com>
2025-08-12 23:50:52 +0530

    misc:removed snippets from source

--- old-nvim/.config/old-nvim/lua/plugins/blink.lua
+++ old-nvim/.config/old-nvim/lua/plugins/blink.lua
@@ -60,7 +60,7 @@ return {
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
-      default = { 'lsp', 'snippets','path', 'buffer' },   --removed snippets
+      default = { 'lsp','path', 'buffer' },   --removed snippets
         providers = {
                     snippets = {
                         -- min_keyword_length = 2,
